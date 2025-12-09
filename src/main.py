5b390ed775e1d7a8c8359a8ab2378f9ca8665dd5
Abba-Max <adjiabba10@gmail.com>
2025-12-09 21:34:16 +0100

    Main function implementation

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from utilities.inventory_manager import InventoryManager
+from ui.console_ui import ConsoleUI
+
+def main():
+    inventory = InventoryManager("data/stock.json")
+    
+    ui = ConsoleUI(inventory)
+    
+    ui.run()
+
+if __name__ == "__main__":
+    main()
