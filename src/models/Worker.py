cec1d0ad4a07a06662a211b3ec2b1d2310335fc0
Abba-Max <adjiabba10@gmail.com>
2025-12-09 19:15:56 +0100

    Worker model updated

--- src/models/Worker.py
+++ src/models/Worker.py
@@ -1,9 +1,9 @@
 class Worker:
     
-    def _init_(self,worker_id:int,name:str,quantity:int,date:str,customer_name:str,products:list):
+    def _init_(self,worker_id:int,name:str):
         self.worker_id = worker_id
         self.name = name
-        self.products = products
-        self.date = date
-        self.customer_name = customer_name
+        
+    def __repr__(self):
+        return f"Worker({self.worker_id}, {self.name})"
        
